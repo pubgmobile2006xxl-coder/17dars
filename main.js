5264369235a43b597ab756492282cbd18b7db0c9
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-04-10 18:36:23 +0500

    2-masala

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-let a =12
-let b =16
-function sum (a,b){
-    p = a + b 
-    s = a * b
-    console.log(p, s);
+// let a =12
+// let b =16
+// function sum (a,b){
+//     p = a + b 
+//     s = a * b
+//     console.log(p, s);
+// }
+// sum(a,b)
+
+let a = 12
+let b = 16
+function sum(a,b){
+    return a +b    
 }
-sum(a,b)
+console.log(sum(a,b));
